271635e29133e3024676dbc82878df2a86246f6f
myselfrabin <shivagaire839@gmail.com>
2025-06-07 13:11:03 +0545

    Added app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,16 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true, limit: "16kb"}))
 app.use(express.urlencoded("public"))
 
+
+// import routes
+import  healthcheckRouter from "./routes/healthcheck.routes.js" 
+import  userinfoRouter from "./routes/userinfo.routes.js" 
+
+
+//routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v2/userinfo", userinfoRouter )
+
 // export this express() app so that we can use it in the somewhere 
 
 export { app }; 
